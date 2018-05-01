7a3349edb0602427677f036e9533f8a7a5c1b77f
root <abhayshekhar.jh@gmail.com>
2018-05-01 19:30:33 +0530

    trial of commands

---
+++ Computer_organization_and_architecture/commands_trial.asm
@@ -0,0 +1,33 @@
+.data 
+
+.text
+
+.globl main
+
+main:
+
+li$t0,1
+
+li $t1,2
+
+li $t2,3
+
+li $t3,4
+
+li $t4,5
+
+li $t0,5
+
+li $t1,6
+
+li $t2,7
+
+li $t3,8
+
+li $t4,9
+
+
+
+li $v0,10
+
+syscall
